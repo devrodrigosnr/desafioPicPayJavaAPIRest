52350179fa4a26cf689dd74f474af27705402edc
Rodrigo <rodrigosnr@hotmail.com>
2024-11-20 23:43:54 -0300

    feat: criação da entidade Transaction

---
+++ src/main/java/com/desafiodevpicpay/domain/transaction/Transaction.java
@@ -0,0 +1,37 @@
+package com.desafiodevpicpay.domain.transaction;
+
+import com.desafiodevpicpay.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name="transaction")
+@Table(name="transaction")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name="receiver_id")
+    private User receiver;
+
+    private LocalDateTime timeStamp;
+
+}
