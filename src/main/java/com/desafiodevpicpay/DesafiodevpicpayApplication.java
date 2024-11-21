a02f20b3044bab55dacecbd1a5cd98a4a4182f19
Rodrigo <rodrigosnr@hotmail.com>
2024-11-20 23:43:03 -0300

    feat: configuração inicial

---
+++ src/main/java/com/desafiodevpicpay/DesafiodevpicpayApplication.java
@@ -0,0 +1,13 @@
+package com.desafiodevpicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiodevpicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiodevpicpayApplication.class, args);
+	}
+
+}
